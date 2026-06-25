f0010a9ce9924faff3612a88769b92e3cfbdf7ce
Keerthi Ramanan <lkeerthanaramanan@gmail.com>
2026-06-25 16:19:23 +0100

    Updated locator constant name and set to merge

--- tests/lists-and-dropdowns.spec.ts
+++ tests/lists-and-dropdowns.spec.ts
@@ -12,7 +12,7 @@ test("Validate selected pet types from the list", async({page}) => {
     await expect(page.getByRole("heading")).toHaveText("Owners")
    
     await page.getByRole("link",{name: "George Franklin"}).click()
-    await expect(page.getByRole("row", {name: 'Name'}).locator("td").nth(0)).toHaveText("George Franklin")
+    await expect(page.locator(".ownerFullName")).toHaveText("George Franklin")
     
     await leoPetSection.getByRole("button",{name: "Edit Pet"}).click()
     await expect(page.getByRole("heading")).toHaveText("Pet")
@@ -30,7 +30,7 @@ test("Validate selected pet types from the list", async({page}) => {
 })
 
 test("Validate the pet type update", async({page}) => {
-    const selectedPetType = page.locator("#type1")
+    const petTypeReadOnlyField = page.locator("#type1")
     const rosyPetSection =  page.locator('app-pet-list',{hasText: "Rosy"})
     await expect(page.getByRole("heading")).toHaveText("Owners")
 
@@ -38,20 +38,20 @@ test("Validate the pet type update", async({page}) => {
     await rosyPetSection.getByRole("button",{name: "Edit Pet"}).click()
 
     await expect(page.locator("#name")).toHaveValue("Rosy")
-    await expect(selectedPetType).toHaveValue("dog")
+    await expect(petTypeReadOnlyField).toHaveValue("dog")
 
     await page.locator("#type").selectOption("bird")
-    await expect(selectedPetType).toHaveValue("bird")
+    await expect(petTypeReadOnlyField).toHaveValue("bird")
     await expect(page.locator("select")).toHaveValue("bird")
 
     await page.getByRole("button",{name: "Update Pet"}).click()
     await expect(rosyPetSection.locator("dd").nth(2)).toHaveText("bird")
 
     await rosyPetSection.getByRole("button",{name: "Edit Pet"}).click()
-    await expect(selectedPetType).toHaveValue("bird")
+    await expect(petTypeReadOnlyField).toHaveValue("bird")
     await page.locator("#type").selectOption("dog")
     await expect(page.locator("select")).toHaveValue("dog")
-    await expect(selectedPetType).toHaveValue("dog")
+    await expect(petTypeReadOnlyField).toHaveValue("dog")
     await page.getByRole("button",{name: "Update Pet"}).click()
     await expect(rosyPetSection.locator("dd").nth(2)).toHaveText("dog") 
 })
