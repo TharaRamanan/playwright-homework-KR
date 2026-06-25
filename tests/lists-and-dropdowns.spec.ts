91a4b6a5a6eeaf17e3e0e075cd6e1ad83505bbc2
Keerthi Ramanan <lkeerthanaramanan@gmail.com>
2026-06-25 13:12:07 +0100

    Removed indexing used in some locators, added constant for locators used more than twice

--- tests/lists-and-dropdowns.spec.ts
+++ tests/lists-and-dropdowns.spec.ts
@@ -4,52 +4,55 @@ test.beforeEach( async({page}) => {
     await page.goto('/')
     await page.getByRole('button', {name: "Owners"}).click()
     await page.getByRole('link', {name: "Search"}).click() 
+    
 })
 
 test("Validate selected pet types from the list", async({page}) => {
+    const leoPetSection =  page.locator('app-pet-list',{hasText: "Leo"})
     await expect(page.getByRole("heading")).toHaveText("Owners")
    
     await page.getByRole("link",{name: "George Franklin"}).click()
-    await expect(page.getByRole('rowheader').nth(0)).toHaveText("Name")
-    await expect(page.getByRole("row").nth(0).locator("td")).toHaveText("George Franklin")
-
-    await page.getByRole("button",{name: "Edit Pet"}).click()
+    await expect(page.getByRole("row", {name: 'Name'}).locator("td").nth(0)).toHaveText("George Franklin")
+    
+    await leoPetSection.getByRole("button",{name: "Edit Pet"}).click()
     await expect(page.getByRole("heading")).toHaveText("Pet")
 
     await expect(page.locator("#owner_name")).toHaveValue("George Franklin")
     await expect(page.locator("#type1")).toHaveValue("cat")
 
-    const petTypes = ["cat","dog","lizard","snake","bird","hamster"]
+    const petTypes = await page.locator("#type option").allTextContents()
     for(const pet of  petTypes ){
         
         await page.locator("#type").selectOption(pet)
         await expect(page.locator("#type1")).toHaveValue(pet)
     }   
+    
 })
 
 test("Validate the pet type update", async({page}) => {
+    const selectedPetType = page.locator("#type1")
+    const rosyPetSection =  page.locator('app-pet-list',{hasText: "Rosy"})
     await expect(page.getByRole("heading")).toHaveText("Owners")
 
     await page.getByRole("link",{name: "Eduardo Rodriquez"}).click()
-    await page.getByRole("button",{name: "Edit Pet"}).nth(1).click()
+    await rosyPetSection.getByRole("button",{name: "Edit Pet"}).click()
 
     await expect(page.locator("#name")).toHaveValue("Rosy")
-    await expect(page.locator("#type1")).toHaveValue("dog")
+    await expect(selectedPetType).toHaveValue("dog")
 
     await page.locator("#type").selectOption("bird")
-    await expect(page.locator("#type1")).toHaveValue("bird")
+    await expect(selectedPetType).toHaveValue("bird")
     await expect(page.locator("select")).toHaveValue("bird")
 
     await page.getByRole("button",{name: "Update Pet"}).click()
+    await expect(rosyPetSection.locator("dd").nth(2)).toHaveText("bird")
 
-    await expect(page.locator("dl", {hasText: "Rosy"}).locator("dd").nth(2)).toHaveText("bird")
-
-    await page.getByRole("button",{name: "Edit Pet"}).nth(1).click()
-    await expect(page.locator("#type1")).toHaveValue("bird")
+    await rosyPetSection.getByRole("button",{name: "Edit Pet"}).click()
+    await expect(selectedPetType).toHaveValue("bird")
     await page.locator("#type").selectOption("dog")
     await expect(page.locator("select")).toHaveValue("dog")
-    await expect(page.locator("#type1")).toHaveValue("dog")
+    await expect(selectedPetType).toHaveValue("dog")
     await page.getByRole("button",{name: "Update Pet"}).click()
-    await expect(page.locator("dl", {hasText: "Rosy"}).locator("dd").nth(2)).toHaveText("dog")   
+    await expect(rosyPetSection.locator("dd").nth(2)).toHaveText("dog") 
 })
 
