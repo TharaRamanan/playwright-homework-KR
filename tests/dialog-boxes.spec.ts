90ca3926eddb4e2a7553af7c92ef037a53f40feb
Keerthi Ramanan <lkeerthanaramanan@gmail.com>
2026-06-25 16:25:50 +0100

    Dialog box - initial commit.  Added test for add and delete pet type

---
+++ tests/dialog-boxes.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach( async({page}) => {
+    await page.goto('/')
+    await page.getByTitle("pettypes").click()
+})
+
+test('Add and delete Pet type', async ({page}) => {
+
+    const newPetTypeSection = page.locator("app-pettype-add")
+    const totalRows = page.locator("table tbody tr")
+    await expect(page.getByRole('heading')).toHaveText('Pet Types')
+    await page.getByRole("button",{name: "Add"}).click()
+
+    await expect(newPetTypeSection.getByRole("heading")).toHaveText("New Pet Type")
+    await expect(newPetTypeSection.locator("label")).toHaveText("Name")
+    await expect(newPetTypeSection.locator("#name")).toBeVisible()
+
+    await newPetTypeSection.locator("#name").fill("pig")
+    await page.getByRole("button",{name: "Save"}).click()
+    await page.waitForTimeout(500)  //timeout added for Save action to complete
+
+    const noTotalRows =  await totalRows.count()
+    console.log(noTotalRows)
+    await expect(page.getByRole("textbox").nth(noTotalRows-1)).toHaveValue("pig")
+    await page.getByRole("button",{name: "Delete"}).nth(noTotalRows-1).click()
+    
+    //page.on('dialog', dialog =>{
+        //dialog.accept()
+   // })
+})
