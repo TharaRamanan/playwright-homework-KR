265454f80bcd07165465dafee6cd9726b6acb340
Keerthi Ramanan <lkeerthanaramanan@gmail.com>
2026-06-29 16:02:11 +0100

    Addressed review comments - removed loop , console.log statement and added waitforResponse after deletion step

--- tests/dialog-boxes.spec.ts
+++ tests/dialog-boxes.spec.ts
@@ -19,22 +19,13 @@ test('Add and delete Pet type', async ({page}) => {
     await newPetTypeSection.getByRole("textbox").fill("pig")
     await page.getByRole("button",{name: "Save"}).click()
     await page.waitForTimeout(500)  //timeout added for Save action to complete
-
     await expect(page.getByRole("textbox").last()).toHaveValue("pig")  
 
-    const totalRowsCount = await page.locator("tbody tr").count()
-    
-    for(let i = totalRowsCount-1;i>=0; i--){
-        const inputValue = await page.locator("tbody tr").nth(i).locator("input").inputValue()
-        console.log(inputValue)
-        if(inputValue == "pig") 
-            {          
-            page.on('dialog', dialog =>{
-            expect(dialog.message()).toEqual("Delete the pet type?")
-            dialog.accept() 
-            })
-        await page.locator("tbody tr").nth(i).getByRole("button",{name: "Delete"}).click()
-        }
-    }
+    page.on('dialog', dialog =>{
+        expect(dialog.message()).toEqual("Delete the pet type?")
+        dialog.accept() 
+    })
+    await page.getByRole("button",{name: "Delete"}).last().click()
+    await page.waitForResponse('**/pettypes/*')
     await expect(page.getByRole("textbox").last()).not.toHaveValue("pig")
 })
