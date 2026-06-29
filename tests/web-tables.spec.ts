3b2cdd6d8044e772b3a567a4dd75702ae21dbb4f
Keerthi Ramanan <lkeerthanaramanan@gmail.com>
2026-06-29 23:11:10 +0100

    Update file with Validate specialty update and Validate pets of Madison city test cases

--- tests/web-tables.spec.ts
+++ tests/web-tables.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+
+test.describe("Pet Owners", () => {
+
 test.beforeEach( async({page}) => {
     await page.goto('/')
     await page.getByRole('button', {name: "Owners"}).click()
@@ -42,10 +45,69 @@ test("Validate search by last name", async({page})=>{
 
 test("Validate phone number and pet name", async({page})=>{
     const ownerPhoneNumberField = page.getByRole("row",{name: "6085552765"})
-    const petName = await ownerPhoneNumberField.locator("td").last().textContent()
+    const petName = await ownerPhoneNumberField.locator("td").last().innerText()
     
     await ownerPhoneNumberField.getByRole("link").click()
+    await expect(page.getByRole("row",{name: "Telephone"}).locator("td")).toHaveText("6085552765")   
     
+    await expect(page.locator("app-pet-list td dd").first()).toHaveText(petName)   
+})
+
+test("Validate pets of Madison city", async({page})=>{
+
+    await page.waitForResponse("**/owners*")
+    const tableRows = page.locator("#ownersTable tbody > tr ")
+
+    const rowCount = await tableRows.count()
+    let petArray = []
     
-    
+    for(let i = 0; i< rowCount; i++){
+
+        const petOwnerColumnsField = page.locator("table tbody > tr").nth(i).locator("td")
+        const cityName = await(petOwnerColumnsField.nth(2)).textContent()
+        if(cityName == "Madison")
+        {
+            const getPetName = await(petOwnerColumnsField.nth(4)).textContent()
+            petArray.push(getPetName)    
+        }    
+    }
+
+expect(petArray).toEqual([' Leo ', ' George ', ' Mulligan ', ' Freddy '])   
+})
+
+})
+
+test("Validate specialty update", async({page})=>{
+    await page.goto('/')
+    await page.getByRole('button',{name: "Veterinarians"}).click()
+    await page.getByRole('link', { name: 'All' }).click()
+
+    const rafaelRowField = page.getByRole("row",{name: "Rafael Ortega"})
+    const editSpecialtyField = page.getByRole("textbox")
+
+    await expect((rafaelRowField).locator("td").nth(1)).toHaveText("surgery")
+
+    await page.getByRole("link",{name: "specialties"}).click()
+    await expect(page.getByRole('heading')).toHaveText("Specialties")
+
+    await page.getByRole("row",{name:"surgery"}).getByRole("button",{name: "Edit"}).click()
+    await expect(page.getByRole('heading')).toHaveText("Edit Specialty")
+    await expect(editSpecialtyField).toHaveValue("surgery")
+
+    await editSpecialtyField.fill("dermatology")
+    await expect(editSpecialtyField).toHaveValue("dermatology")
+    await page.getByRole("button", {name: "Update"}).click()
+    await expect(page.getByRole("row").nth(1).getByRole("textbox")).toHaveValue("dermatology")
+
+    await page.getByRole('button',{name: "Veterinarians"}).click()
+    await page.getByRole('link', { name: 'All' }).click()
+    await expect((rafaelRowField).locator("td").nth(1)).toHaveText("dermatology")
+
+    await page.getByRole("link",{name: "specialties"}).click()
+    await page.getByRole("row",{name:"dermatology"}).getByRole("button",{name: "Edit"}).click()
+    await expect(editSpecialtyField).toHaveValue("dermatology")   //waitforResponse('**/specialties/*/edit')) did not work, why ?
+    await editSpecialtyField.fill("surgery")
+    await expect(editSpecialtyField).toHaveValue("surgery") 
+    await page.getByRole("button", {name: "Update"}).click()
+   
 })
