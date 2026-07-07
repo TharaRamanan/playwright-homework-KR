477d2865439ea176d6f971364348858eecae40cc
Keerthi Ramanan <lkeerthanaramanan@gmail.com>
2026-07-07 16:38:11 +0100

    Completed test case and added required test data json files

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
-import ownersObj from '../test-data/owners.json'
+import ownersObj from '../test-data/ownersList.json'
+import firstOwnerInfoObj from '../test-data/ownerKyaClarkInfo.json'
 
 test.beforeEach(async ({ page }) => {
     await page.route("**/api/owners", async route => {
@@ -16,7 +17,29 @@ test.beforeEach(async ({ page }) => {
 
 })
 
-test('Mocking API Response', async ({ page }) => {
-    await expect(page.getByRole("heading")).toHaveText("Owners")
+test('mocking API request', async ({ page }) => {
+    await expect(page.locator("#ownersTable").locator("tbody > tr")).toHaveCount(2)
+    await page.route("**/api/owners/*", async route => {
+        await route.fulfill({
+            contentType: "application/json",
+            body: JSON.stringify(firstOwnerInfoObj)
+        })
+
+    })
+    await page.getByRole("link", { name: "Kya Clark" }).click()
+    //await expect(page.locator(".ownerFullName")).toHaveText("Kya Clark")
+    const ownerKyaClarkDetails = ["Kya Clark", "Quality Street", "Cardiff", "9789090475"]
+    let i = 0
+    for (let info of ownerKyaClarkDetails) {
+
+        await expect(page.locator("app-owner-detail table").first().locator("td").nth(i)).toHaveText(info)
+        i = i + 1
+
+    }
+    await expect(page.locator("app-pet-list")).toHaveCount(2)
+    await expect(page.locator("app-pet-list", { hasText: "cat" }).locator("dd").first()).toHaveText("Piper")
+    await expect(page.locator("app-pet-list", { hasText: "dog" }).locator("dd").first()).toHaveText("Milo")
+
+    await expect(page.locator("app-pet-list", { hasText: "Piper" }).locator("app-visit-list table > tr")).toHaveCount(10)
 
 })
