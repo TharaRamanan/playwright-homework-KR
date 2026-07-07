eda503268e97c7cbde3bff1541f58b4c41575056
Keerthi Ramanan <lkeerthanaramanan@gmail.com>
2026-07-07 13:53:11 +0100

    Initial Commit - Mocking API response

---
+++ tests/working-with-api.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test'
+import ownersObj from '../test-data/owners.json'
+
+test.beforeEach(async ({ page }) => {
+    await page.route("**/api/owners", async route => {
+        await route.fulfill({
+            contentType: "application/json",
+            body: JSON.stringify(ownersObj)
+        })
+
+    })
+
+await page.goto('/')
+await page.getByRole('button', {name: "Owners"}).click()
+await page.getByRole('link', {name: "Search"}).click() 
+
+})
+
+test('Mocking API Response', async ({ page }) => {
+    await expect(page.getByRole("heading")).toHaveText("Owners")
+
+})
