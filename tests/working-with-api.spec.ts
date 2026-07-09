8c9126573e399a068629cf6079a33339b0f6b4ff
Keerthi Ramanan <lkeerthanaramanan@gmail.com>
2026-07-09 12:10:29 +0100

    Refactored code to dynamically update array object

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test'
 import ownersObj from '../test-data/ownersList.json'
 import kyaClarkInfoObj from '../test-data/ownerKyaClarkInfo.json'
+import sharonJenkinsSpecialtiesObj from '../test-data/vetSharonJenkinsSpecialties.json'
 
 test.describe("Mocking Owner info response", () => {
     test.beforeEach(async ({ page }) => {
@@ -18,25 +19,25 @@ test.describe("Mocking Owner info response", () => {
 
     })
 
-test('mocking API request', async ({ page }) => {
-    await expect(page.locator("#ownersTable").locator("tbody > tr")).toHaveCount(2)
-    await page.route("**/api/owners/*", async route => {
-        await route.fulfill({
-            contentType: "application/json",
-            body: JSON.stringify(kyaClarkInfoObj)
+    test('mocking API request', async ({ page }) => {
+        await expect(page.locator("#ownersTable").locator("tbody > tr")).toHaveCount(2)
+        await page.route("**/api/owners/*", async route => {
+            await route.fulfill({
+                contentType: "application/json",
+                body: JSON.stringify(kyaClarkInfoObj)
+            })
+
         })
-
-    })
-    await page.getByRole("link", { name: "Kya Clark" }).click()
-    await expect(page.getByRole("row",{name: "Name"}).first()).toContainText(`${kyaClarkInfoObj.firstName} ${kyaClarkInfoObj.lastName}`)
-    await expect(page.getByRole("row",{name: "Address"})).toContainText(kyaClarkInfoObj.address)
-    await expect(page.getByRole("row",{name: "City"})).toContainText(kyaClarkInfoObj.city)
-    await expect(page.getByRole("row",{name: "Telephone"})).toContainText(kyaClarkInfoObj.telephone)
+        await page.getByRole("link", { name: "Kya Clark" }).click()
+        await expect(page.getByRole("row", { name: "Name" }).first()).toContainText(`${kyaClarkInfoObj.firstName} ${kyaClarkInfoObj.lastName}`)
+        await expect(page.getByRole("row", { name: "Address" })).toContainText(kyaClarkInfoObj.address)
+        await expect(page.getByRole("row", { name: "City" })).toContainText(kyaClarkInfoObj.city)
+        await expect(page.getByRole("row", { name: "Telephone" })).toContainText(kyaClarkInfoObj.telephone)
 
 
-    await expect(page.locator("app-pet-list")).toHaveCount(2)
-    await expect(page.locator("app-pet-list", { hasText: "cat" }).locator("dd").first()).toHaveText("Piper")
-    await expect(page.locator("app-pet-list", { hasText: "dog" }).locator("dd").first()).toHaveText("Milo")
+        await expect(page.locator("app-pet-list")).toHaveCount(2)
+        await expect(page.locator("app-pet-list", { hasText: "cat" }).locator("dd").first()).toHaveText("Piper")
+        await expect(page.locator("app-pet-list", { hasText: "dog" }).locator("dd").first()).toHaveText("Milo")
 
         await expect(page.locator("app-pet-list", { hasText: "Piper" }).locator("app-visit-list table > tr")).toHaveCount(10)
 
@@ -46,30 +47,25 @@ test('mocking API request', async ({ page }) => {
 test('Intercept API response', async ({ page }) => {
     await page.route('*/**/api/vets', async route => {
         const interceptedResponse = await route.fetch()
-        const responseBody = await interceptedResponse.json()
-        responseBody[5].specialties = [
-            { id: 9991, name: "dermatology" },
-            { id: 9992, name: "surgery" },
-            { id: 9993, name: "grooming" },
-            { id: 9994, name: "dentistry" },
-            { id: 9995, name: "therapy" },
-            { id: 9996, name: "radiology" },
-            { id: 9997, name: "oncology" },
-            { id: 9998, name: "nutrition" },
-            { id: 9999, name: "cardiology" },
-            { id: 9990, name: "neurology" }
-        ]
-
+        const responseBodyJson = await interceptedResponse.json()
+        const updatedSpecialtiesResponseJson = responseBodyJson.map(vet => {
+            if (vet.firstName === 'Sharon' && vet.lastName === 'Jenkins') {
+                return {
+                    ...vet, specialties: sharonJenkinsSpecialtiesObj
+                }
+            }
+            return vet
+        })
         await route.fulfill({
-            body: JSON.stringify(responseBody)
+            body: JSON.stringify(updatedSpecialtiesResponseJson)
         })
     })
 
     await page.goto('/')
     await page.getByRole('button', { name: "Veterinarians" }).click()
     await page.getByRole('link', { name: 'All' }).click()
-    const sharonSpecialties = ["dermatology", "surgery", "grooming", "dentistry", "therapy", "radiology", "oncology", "nutrition", "cardiology", "neurology"]
-    for (const [i, specialties] of sharonSpecialties.entries()) {
-        await expect(page.locator("tr", { hasText: "Sharon Jenkins" }).locator("td").nth(1).locator("div").nth(i)).toHaveText(specialties)
+
+    for (let i = 0; i < sharonJenkinsSpecialtiesObj.length; i++) {
+        await expect(page.locator("tr", { hasText: "Sharon Jenkins" }).locator("td").nth(1).locator("div").nth(i)).toHaveText(sharonJenkinsSpecialtiesObj[i].name)
     }
 })
